1debf2d7e61b10622c8692ca0e7329eeeab0d12f
Bryan <wangyu@qfpay.com>
2019-09-27 16:21:50 +0800

    添加处方界面

--- wx_xcx/pages/add_prescription/index.js
+++ wx_xcx/pages/add_prescription/index.js
@@ -5,65 +5,109 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    selected_item:{},
+    mydata:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
+    this.getdataList();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  goPrescDetail: function () {
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+    this.data.selected_item=mydata[e.detail.value-1];
+    
+  },
+  getdataList() {
+    let _this = this;
+    console.log("getList");
+    wx.showLoading({
+      title: '数据加载中...',
+      mask: true
+    })
+    wx.request({
+      url: 'http://app.uyu.com:7100/store/v1/api/obstacle_type/list',
+      method: 'GET',
+      data: {
+        os: 'xiaochengxu',
+        app_version: '1.0.0'
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'cookie': wx.getStorageSync("sessionid")
+      },
+      success(res) {
+        console.log(res.data);
+        if (res.data.respcd == '0000') {
+
+          _this.mydata = res.data.data.obstacles;
+
+          _this.setData({
+
+
+            mydata: res.data.data.obstacles
+          })
+        }
+      },
+      complete() {
+        console.log("complete");
+        wx.hideLoading();
+      }
+    });
+  },
+  goPrescDetail: function() {
     wx.navigateTo({
       url: '/pages/prescription_detail/index',
     })
